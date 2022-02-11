ca2c5370107c720ed662a715ea1af9a59ac0fa94
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2022-02-11 16:01:17 +0700

    add bottom navigation

--- app/src/main/java/com/ogif/kotae/ui/main/UserActivity.java
+++ app/src/main/java/com/ogif/kotae/ui/main/UserActivity.java
@@ -1,9 +1,10 @@
 package com.ogif.kotae.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ogif.kotae.R;
 
 public class UserActivity extends AppCompatActivity {
@@ -12,5 +13,40 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        // initialize home screen
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView bottomNav = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.page_home:
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    break;
+                case R.id.page_favorite:
+                    fragment = new FavoriteFragment();
+                    loadFragment(fragment);
+                    break;
+                case R.id.page_question:
+                    fragment = new MyQuestionFragment();
+                    loadFragment(fragment);
+                    break;
+                case R.id.page_profile:
+                    fragment = new ProfileFragment();
+                    loadFragment(fragment);
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
