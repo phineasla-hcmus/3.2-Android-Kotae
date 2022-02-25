697e50a74618720290054309c976c2db26733942
nguyenthuthaochau2201 <63655372+nguyenthuthaochau2201@users.noreply.github.com>
2022-02-25 09:26:53 +0700

    question class

---
+++ app/src/main/java/com/ogif/kotae/data/model/Question.java
@@ -0,0 +1,45 @@
+package com.ogif.kotae.data.model;
+
+public class Question {
+    private String content;
+    private String author;
+    private  int upvote;
+    private int downvote;
+    private int report;
+    private String subject;
+    private String grade;
+
+    public Question(String author, String grade, String subject, String content){
+        this.author = author;
+        this.grade = grade;
+        this.content = content;
+        this.subject = subject;
+        this.upvote = 0;
+        this.downvote = 0;
+        this.report = 0;
+    }
+
+    public String getAuthor() { return this.author;}
+
+    public String getQuestionContent() { return this.content; }
+
+    public Question setQuestionContent(String content){
+        this.content = content;
+        return this;
+    }
+
+    public Question setUpvote(int upvote) {
+        this.upvote = upvote;
+        return this;
+    }
+
+    public Question setDownvote(int downvote){
+        this.downvote = downvote;
+        return this;
+    }
+
+    public Question setReport(int report){
+        this.report = report;
+        return this;
+    }
+}
