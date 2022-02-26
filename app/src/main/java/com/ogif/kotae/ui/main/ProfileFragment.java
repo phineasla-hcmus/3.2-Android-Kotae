983bc165d8d28596b3634c21e1320027cd866c9e
phineasla <63248715+phineasla@users.noreply.github.com>
2022-02-26 14:22:32 +0700

    Touch up fragment_profile.xml

--- app/src/main/java/com/ogif/kotae/ui/main/ProfileFragment.java
+++ app/src/main/java/com/ogif/kotae/ui/main/ProfileFragment.java
@@ -3,20 +3,26 @@ package com.ogif.kotae.ui.main;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.ogif.kotae.R;
+import com.ogif.kotae.data.DarkModePreferenceManager;
+
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -68,12 +74,38 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+        Objects.requireNonNull(((AppCompatActivity) requireActivity())
+                .getSupportActionBar())
+                .hide();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        ((AppCompatActivity) getActivity()).getSupportActionBar().show();
+        Objects.requireNonNull(((AppCompatActivity) requireActivity())
+                .getSupportActionBar())
+                .show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.switch_profile_dark_mode) {
+            SwitchMaterial darkMode = (SwitchMaterial) v;
+            DarkModePreferenceManager manager = new DarkModePreferenceManager(this.requireContext());
+            darkMode.setChecked(manager.isDarkMode());
+            darkMode.setOnCheckedChangeListener((compoundButton, b) -> {
+                manager.setDarkMode(!manager.isDarkMode());
+                // TODO LOL
+                // https://stackoverflow.com/questions/61234417/setting-daynight-theme-using-appcompatdelegate-setdefaultnightmode-not-working
+                AppCompatDelegate.setDefaultNightMode(
+                        AppCompatDelegate.MODE_NIGHT_YES
+                );
+            });
+        } else if (id == R.id.tv_profile_language) {
+
+        } else if (id == R.id.tv_profile_change_password) {
+
+        }
     }
 }
