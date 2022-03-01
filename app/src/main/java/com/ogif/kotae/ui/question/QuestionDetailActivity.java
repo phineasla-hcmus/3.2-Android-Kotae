7c974f488d158531c42179f4e5ddc8a2c4414ecc
phineasla <63248715+phineasla@users.noreply.github.com>
2022-03-01 23:19:24 +0700

    Add activity_question_detail.xml

---
+++ app/src/main/java/com/ogif/kotae/ui/question/QuestionDetailActivity.java
@@ -0,0 +1,16 @@
+package com.ogif.kotae.ui.question;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ogif.kotae.R;
+
+public class QuestionDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_detail);
+    }
+}
