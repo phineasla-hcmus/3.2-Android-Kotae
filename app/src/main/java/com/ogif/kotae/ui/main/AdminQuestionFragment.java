fc77991f502c6ef9d5be152a127486aab7624d00
Nguyễn Thành Đạt <52274820+changkho6310@users.noreply.github.com>
2022-03-25 20:50:45 +0700

    add listview question to admin

--- app/src/main/java/com/ogif/kotae/ui/main/AdminQuestionFragment.java
+++ app/src/main/java/com/ogif/kotae/ui/main/AdminQuestionFragment.java
@@ -2,13 +2,29 @@ package com.ogif.kotae.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.ogif.kotae.R;
+import com.ogif.kotae.data.model.Question;
+import com.ogif.kotae.data.model.User;
+import com.ogif.kotae.utils.AdminQuestionAdapter;
+import com.ogif.kotae.utils.AdminUserAdapter;
+
+import java.util.ArrayList;
 
 public class AdminQuestionFragment extends Fragment {
 
@@ -24,6 +40,33 @@ public class AdminQuestionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_admin_question, container, false);
+        View view = inflater.inflate(R.layout.fragment_admin_question, container, false);
+
+        ListView lvQuestion = (ListView) view.findViewById(R.id.lv_question_admin);
+
+        ArrayList<Question> questionArrayList = new ArrayList<Question>();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Query queryQuestion = db.collection("questions").orderBy("report", Query.Direction.DESCENDING);
+        queryQuestion.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                    Question question = documentSnapshot.toObject(Question.class);
+                    questionArrayList.add(question);
+                }
+
+                AdminQuestionAdapter questionAdapter = new AdminQuestionAdapter(getActivity(), R.layout.item_question, questionArrayList);
+                lvQuestion.setAdapter(questionAdapter);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("AAA", e.toString());
+            }
+        });
+
+
+        return view;
     }
 }
--- app/src/main/java/com/ogif/kotae/utils/AdminQuestionAdapter.java
+++ app/src/main/java/com/ogif/kotae/utils/AdminQuestionAdapter.java
@@ -5,10 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.material.chip.Chip;
+import com.ogif.kotae.R;
 import com.ogif.kotae.data.model.Question;
+import com.ogif.kotae.data.model.User;
 
 import java.util.ArrayList;
 
@@ -18,6 +23,12 @@ public class AdminQuestionAdapter extends BaseAdapter {
     private int layout;
     private ArrayList<Question> questionArrayList;
 
+
+    public AdminQuestionAdapter(Context context, int layout, ArrayList<Question> questionArrayList) {
+        this.context = context;
+        this.layout = layout;
+        this.questionArrayList = questionArrayList;
+    }
 
     @Override
     public int getCount() {
@@ -37,10 +48,34 @@ public class AdminQuestionAdapter extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
         view = inflater.inflate(layout, null);
 
-        
-        return null;
+        TextView tvQuestionTitle = (TextView) view.findViewById(R.id.tv_question_title);
+        TextView tvQuestionContent = (TextView) view.findViewById(R.id.tv_question_content);
+        TextView tvQuestionPosttime = (TextView) view.findViewById(R.id.tv_question_post_time);
+        TextView tvQuestionAuthor = (TextView) view.findViewById(R.id.tv_author);
+        TextView tvQuestionUpvote = (TextView) view.findViewById(R.id.tv_up);
+        TextView tvQuestionDownvote= (TextView) view.findViewById(R.id.tv_down);
+        TextView tvQuestionReport= (TextView) view.findViewById(R.id.tv_report);
+        Chip chipSubject = (Chip) view.findViewById(R.id.chip_subject);
+        Chip chipGrade = (Chip) view.findViewById(R.id.chip_grade);
+        ImageButton ibBlock = (ImageButton) view.findViewById(R.id.ib_block);
+
+        Question question = questionArrayList.get(i);
+        tvQuestionTitle.setText(question.getTitle());
+        tvQuestionContent.setText(question.getContent());
+//        tvQuestionPosttime.setText(question.getPostTime().toString());
+        tvQuestionAuthor.setText(question.getAuthor());
+        tvQuestionUpvote.setText(String.valueOf(question.getUpvote()));
+        tvQuestionDownvote.setText(String.valueOf(question.getDownvote()));
+        tvQuestionReport.setText(String.valueOf(question.getReport()));
+        chipSubject.setText(question.getSubject());
+        chipGrade.setText(question.getGrade());
+        ibBlock.setVisibility(View.VISIBLE);
+        if(question.isBlocked()){
+           ibBlock.setColorFilter(R.color.design_default_color_error);
+        }
+
+        return view;
     }
 }
